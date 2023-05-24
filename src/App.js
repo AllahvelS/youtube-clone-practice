844a78f29700562907f5e663b18cbe5b21fede06
AllahvelS <allahvelsalisbury@pursuit.org>
2023-05-23 20:37:54 -0400

    Created Components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+import { useState } from 'react';
+
+
+//Components
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import SearchBar from "./components/Searchbar";
+
+//Css
 import './App.css';
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <div>
+            <h2>Youtube</h2>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+            </ul>
+        </div>
+    )
+}

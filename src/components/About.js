15eba246cc0befd96f3436ecda56b21036e02726
AllahvelS <allahvelsalisbury@pursuit.org>
2023-05-31 20:22:39 -0400

    Testing push.

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>About</h2>
-      <p>This is the about page.</p>
+      <h2>The Crew</h2>
+
+      <h3>Frantz</h3>
+      <p>My name is Frantz-Sebastien, I'm from Haiti and I came to the US in 2007. My passions involves Nature, Politics and History. I'm currently learning to be a Software Engineer so that I can use those skills to save the planet! My GitHub page can be found here: https://github.com/Frantz-Sebastien</p>
+
+      <h3>Sabri</h3>
+      <p>"Hi everyone. My name is Sabri Mohiuddin. I am from Bangladesh. I moved to America about 10 years ago with my family. Even though my background is not in the tech field, I have recently started my journey to become a full-stack software engineer. My hobbies include fish keeping, gaining knowledge about the stock market and real estate. Here is my GitHub page link: https://github.com/sabrimohiuddin."</p>
+
+      <h3>Abel</h3>
+      <p>My name is Abel Vargas. I'm passionate about tabletop gaming and theatre. I'm currently training to be a software engineer.</p>
     </div>
   );
 };

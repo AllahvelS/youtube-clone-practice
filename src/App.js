925f3ce3be4c795d200ef755fc7154e61dc288b5
Sabri Mohiuddin <you@example.com>
2023-06-04 02:38:13 -0400

    ggggg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
+// import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+// import { useState } from 'react';
+
+
+// //Components
+// import Header from './Components/Header';
+// import Home from './Components/Home'
+// import About from './Components/About'
+// import SearchBar from './Components/Searchbar';
+
+// //Css
+// import './App.css';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Header />
+//         {/* Define your routes using the Routes component */}
+//         <Routes>
+//           {/* Define individual routes using the Route component */}
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//       </div>
+//       <SearchBar />
+//     </Router>
+//   );
+// }
+
+
+// export default App;
+
+
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { useState } from 'react';
 
+// Components
+import Header from './components/Header'  ;
+import Home from  './components/Home';
+import About from './components/About';
+import SearchBar from './components/Searchbar';
+import Videos from './components/Videos';  // Import Videos component
 
 // Css
 import './App.css';
@@ -28,6 +69,7 @@ function App() {
 }
 
 export default App;
+
 
 
 

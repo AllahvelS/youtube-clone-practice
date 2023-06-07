7a8cec21607bd4e3c755db80467463b730131b6b
Sabri Mohiuddin <you@example.com>
2023-06-06 20:04:40 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -6,28 +6,40 @@ import Home from  './components/Home';
 import About from './components/About';
 import Videos from './components/Videos';
 import VideoPlayer from './components/VideoPlayer'; 
+import ModalWindow from './components/ModalWindow';
+
 
 import './App.css';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
 
   return (
     <Router>
       <div>
         <Header />
+        <button className="open-modal-btn" onClick={() => setIsModalOpen(true)}>Open Modal</button>
+
+
         <Routes>
           <Route path="/" element={<Home setVideos={setVideos} />} />
           <Route path="/about" element={<About />} />
           <Route path="/search" element={<Videos videos={videos} setVideos={setVideos} />} /> 
+
           <Route path="/videos/:videoId" element={<VideoPlayer />} />
         </Routes>
+
+        {isModalOpen && <ModalWindow onClose={() => setIsModalOpen(false)} />}
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
 
 
 

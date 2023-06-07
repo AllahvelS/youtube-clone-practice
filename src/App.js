cf2428e7c8d28dd919232745d499dedd1ac93b65
Sabri Mohiuddin <you@example.com>
2023-06-06 20:04:40 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Home from  './components/Home';
 import About from './components/About';
 import Videos from './components/Videos';
 import VideoPlayer from './components/VideoPlayer'; 
+import ModalWindow from './components/ModalWindow';
+
 
 
 
@@ -15,23 +17,33 @@ import './App.css';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
 
   return (
     <Router>
       <div>
         <Header />
+        <button className="open-modal-btn" onClick={() => setIsModalOpen(true)}>Open Modal</button>
+
+
         <Routes>
           <Route path="/" element={<Home setVideos={setVideos} />} />
           <Route path="/about" element={<About />} />
           <Route path="/search" element={<Videos videos={videos} setVideos={setVideos} />} /> 
+
           <Route path="/videos/:videoId" element={<VideoPlayer />} />
         </Routes>
+
+        {isModalOpen && <ModalWindow onClose={() => setIsModalOpen(false)} />}
       </div>
     </Router>
   );
 }
 
 export default App;
+
+
 
 
 
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,24 +2,30 @@
 import { useState } from "react";
 import "./Searchbar.css"
 import { useNavigate } from 'react-router-dom'; 
+import ModalWindow from "./ModalWindow";
 
 export const SearchBar = ({ setVideos, videos, disableMessage }) => {
+ 
     const [searchTerm, setSearchTerm] = useState("");
-    const [maxResults, setMaxResults] = useState(10); 
     const navigate = useNavigate(); 
 
     let handleSearch = (event) => {
         event.preventDefault();
         let query = searchTerm ? searchTerm : "trending";
      fetch(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${process.env.REACT_APP_API_KEY}&part=snippet&type=video&maxResults=20`)
-        .then(response => response.json())
+
+        .then (response => response.json())   
         .then((response) => {
             console.log("Response:");
             console.log(response.items);
             setVideos(response.items);
             navigate("/search"); 
+        }) 
+        .catch ((error) => {
+            <ModalWindow/>
+        } )  
 
-        });
+        
     };
 
     return (
@@ -30,14 +36,7 @@ export const SearchBar = ({ setVideos, videos, disableMessage }) => {
                 placeholder="Search Video"
                 type="text"
             /> 
-            <input
-                onChange={event => setMaxResults(event.target.value)}
-                value={maxResults}
-                type="number"
-                min="0"
-                max="50"
-                placeholder="Max Results"
-            />
+       
             <button className="search-button" onClick={handleSearch}>
                 <i className="material-icons">Search</i>
             </button>
